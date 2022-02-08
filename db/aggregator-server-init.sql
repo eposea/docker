ad5db1ccc1b1c7e4cc04e80468984b33642d53fa
blackkiv <fallena238@gmail.com>
2022-02-08 22:54:33 +0200

    add aggregator_database and institutions_database

---
+++ db/aggregator-server-init.sql
@@ -0,0 +1,6 @@
+\connect root root;
+CREATE DATABASE aggregator_database WITH OWNER = root
+    ENCODING = 'UTF8';  
+
+CREATE DATABASE institutions_database WITH OWNER = root
+    ENCODING = 'UTF8';  
